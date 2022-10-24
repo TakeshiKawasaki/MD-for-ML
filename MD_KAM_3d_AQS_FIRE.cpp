513d0242d299a9594df71de4dbf61172189044a5
Takeshi Kawasaki <72239760+TakeshiKawasaki@users.noreply.github.com>
2022-10-24 09:59:47 +0900

    Update MD_KAM_3d_AQS_FIRE.cpp

--- MD_KAM_3d_AQS_FIRE.cpp
+++ MD_KAM_3d_AQS_FIRE.cpp
@@ -191,8 +191,8 @@ void calc_force_hs(double (*x)[dim],double (*f)[dim],int *a,double *U,int (*list
       if(a[i]+a[list[i][j]] == 2){
 	aij=1.0;
 	eij=1.0;
-  //      1:0 0:8 0:8
-  //	  1:0 0:5 1:5
+  //      1.0 0.8 0.88
+  //	  1.0 1.5 0.5
       }
       if(a[i]+a[list[i][j]] == 3){
 	aij=0.8;
